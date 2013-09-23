9df1805db2283ce150e97d9bafbd9035c32cedd2
matt-tyler <matt.tyler@flashics.com>
2013-09-23 19:14:10 +0800

    Upgrade fieldtracker to use directives

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','restangular']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partial/1', controller: fieldCtrl});
+    $routeProvider.when('/view1', {templateUrl: 'partial/1'});
     $routeProvider.otherwise({redirectTo: '/view1'});
     $locationProvider.html5Mode(true);
   }]);
--- public/js/directives.js
+++ public/js/directives.js
@@ -3,25 +3,74 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+var fieldtrackerDirective = angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
-/*
-directive('', function factory() {
-  var directiveDefinitionObject = {
-      scope: {
 
-      },
-      compile:
-  function compile(tElement, tAttrs, transclude) {
+fieldtrackerDirective.controller('fieldtrackerCtrl',function($scope,Restangular) {
 
-    return function (scope, element, attrs) {
+    var states = ['sufficient','insufficient','absent']
+
+    $scope.changeState = function(index,week) {
+
+      var state = states.indexOf($scope.records[index]["history"][week]);
+
+      if(state + 1  < states.length) {
+        $scope.records[index]["history"][week] = states[state+1];
+      }
+      else {
+        $scope.records[index]["history"][week] = states[0];
+      }
+      var name = $scope.records[index].name
+      var status = $scope.records[index]["history"][week];
+
+      Restangular.one("api/name",name).one("week",week).one("status",status).put();
     }
-  }
-};
-return directiveDefinitionObject;
-})
-  */
+
+    Restangular.all('api/records').getList().then(function (accounts) {
+      $scope.records = accounts;
+    });
+
+    $scope.range = [];
+    for(var i = 1; i < 54; i++){
+      $scope.range.push(i);
+    }
+
+    Restangular.one('api/page','').get().then(function (response) {
+      $scope.from = parseInt(response.page);
+      $scope.listnum = 15;
+    });
+
+    $scope.nextPage = function() {
+      $scope.from = $scope.from + 15;
+      if($scope.from > 38) {
+        $scope.from = 38;
+      }
+      Restangular.one('api/page',$scope.from).put();
+    };
+
+    $scope.prevPage = function() {
+      $scope.from = $scope.from - 15;
+      if($scope.from < 0) {
+        $scope.from = 0;
+      }
+      Restangular.one('api/page',$scope.from).put();
+    };
+});
+
+fieldtrackerDirective.directive('fieldtracker',function() {
+    return {
+      restrict: 'EA',
+      replace: true,
+      controller: 'fieldtrackerCtrl',
+      scope: {},
+      templateUrl: 'partial/3',
+      compile: function(tElement,tAttrs){
+        return function link(scope,iElement,iAttrs,ctrl) {
+        }
+      }
+    }
+  })
